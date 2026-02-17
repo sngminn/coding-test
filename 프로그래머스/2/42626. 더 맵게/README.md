3cce456196d4507eab4bee8db74a5f473d98a1b3
sngmin <134246428+sngminn@users.noreply.github.com>
2026-02-17 21:43:39 +0900

    [level 2] Title: 더 맵게, Time: 651.20 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=javascript) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 866.98 ms
+메모리: 101 MB, 시간: 651.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 16일 15:22:15
+2026년 02월 17일 21:43:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,56 +1,82 @@
-const heap = [];
-
-function push(value) {
-	heap.push(value);
-    let pointer = heap.length - 1;
-    while(true) {
-		const prev = Math.floor((pointer - 1) / 2);
-        if (heap[prev] && heap[pointer] < heap[prev]){
-	        [heap[pointer], heap[prev]] = [heap[prev], heap[pointer]];
-        } else break;
-        pointer = prev;
-    }
-}
-
-function pop() {
-    const len = heap.length;
-    if (len === 0) return null;
-	if (len === 1) return heap.pop();
-    
-    const result = heap[0];
-    heap[0] = heap.pop();
-    
-    let pointer = 0;
-    
-    while(true) {
-        let smallest = pointer;
-        const left = pointer * 2 + 1;
-        const right = pointer * 2 + 2;
-        
-        if (left < len && heap[left] < heap[smallest]) smallest = left;
-        if (right < len && heap[right] < heap[smallest]) smallest = right;
-        
-        if (smallest === pointer) break;
-        
-        [heap[pointer], heap[smallest]] = [heap[smallest], heap[pointer]];
-        pointer = smallest;
+class MinHeap {
+    constructor() {
+        this.heap = [];
     }
     
-    return result;
+    size() {
+        return this.heap.length;
+    }
+    
+    peek() {
+        return this.heap[0];
+    }
+    
+    push(value) {
+        this.heap.push(value);
+        let currentIndex = this.heap.length - 1;
+        
+        while(currentIndex > 0) {
+            const parentIndex = Math.floor((currentIndex - 1) / 2);
+            if (this.heap[currentIndex] < this.heap[parentIndex]) {
+				[this.heap[currentIndex], this.heap[parentIndex]] =
+				[this.heap[parentIndex], this.heap[currentIndex]];
+                
+                currentIndex = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    
+    pop() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+        
+        const minValue = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        let currentIndex = 0;
+        
+        while(true) {
+            let smallestIndex = currentIndex;
+            const leftIndex = currentIndex * 2 + 1;
+            const rightIndex = currentIndex * 2 + 2;
+            
+            if (leftIndex < this.heap.length && this.heap[leftIndex] < this.heap[smallestIndex]) {
+                smallestIndex = leftIndex;
+            }
+            if (rightIndex < this.heap.length && this.heap[rightIndex] < this.heap[smallestIndex]) {
+                smallestIndex = rightIndex;
+            }
+            if (smallestIndex !== currentIndex) {
+                [this.heap[currentIndex], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[currentIndex]];
+                currentIndex = smallestIndex;
+            } else {
+                break;
+            }
+        }
+        
+        return minValue;
+    }
 }
 
 function solution(scoville, K) {
-    for(s of scoville) push(s);
-    let pointer = 0;
+    const minHeap = new MinHeap();
+    
+    for(const s of scoville) {
+        minHeap.push(s);
+    }
+    
     let count = 0;
     
-    while(heap.length >= 2 && heap[0] < K) {
-        const fir = pop();
-        const sec = pop();
-        const sum = fir + sec * 2;
-        push(sum);
-        count ++;
+    while (minHeap.size() >= 2 && minHeap.peek() < K) {
+        const first = minHeap.pop();
+        const second = minHeap.pop();
+        
+        const mixed = first + (second * 2);
+        minHeap.push(mixed);
+        
+        count++;
     }
-
-    return heap[0] >= K ? count : -1;
+    
+    return minHeap.peek() >= K ? count : -1
 }
